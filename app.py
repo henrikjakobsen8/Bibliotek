0464ea92d29162b5c0ab03138bd40b4621011a38
henrikjakobsen8 <henrikjakobsen8@hotmail.com>
2025-06-28 20:26:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -218,3 +218,7 @@ def admin_login():
           {% endif %}
         {% endwith %}
     ''')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
