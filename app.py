d952adb073b8bba515a2cc2d4c534f41d321df6f
henrikjakobsen8 <henrikjakobsen8@hotmail.com>
2025-06-29 08:30:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -226,4 +226,5 @@ def opret_bog():
     return redirect(url_for('admin'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
